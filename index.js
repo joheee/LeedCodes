271cf6525112d5aba02527e3e632010d1b997b84
joheee <94522173+joheee@users.noreply.github.com>
2023-07-19 02:40:18 +0700

    2: acc!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const _79_WordSearch_1 = require("./code/79_WordSearch");
+const _2_AddTwoNumbers_1 = require("./code/2_AddTwoNumbers");
 // AddTwoNumber()
-(0, _79_WordSearch_1.WordSearch)();
+(0, _2_AddTwoNumbers_1.AddTwoNumbers)();
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
+import { AddTwoNumbers } from "./code/2_AddTwoNumbers";
 import { AddTwoNumber } from "./code/445_AddTwoNumber";
 import { WordSearch } from "./code/79_WordSearch";
 
 // AddTwoNumber()
-WordSearch()
+AddTwoNumbers()

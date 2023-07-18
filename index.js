0be4aeae174be3d35a5b0497b58fe02023f51272
joheee <94522173+joheee@users.noreply.github.com>
2023-07-19 03:03:25 +0700

    7: acc!

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const _2_AddTwoNumbers_1 = require("./code/2_AddTwoNumbers");
-// AddTwoNumber()
-(0, _2_AddTwoNumbers_1.AddTwoNumbers)();
+const _7_ReverseInteger_1 = require("./code/7_ReverseInteger");
+(0, _7_ReverseInteger_1.ReverseInteger)();
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { AddTwoNumbers } from "./code/2_AddTwoNumbers";
 import { AddTwoNumber } from "./code/445_AddTwoNumber";
 import { WordSearch } from "./code/79_WordSearch";
+import { ReverseInteger } from "./code/7_ReverseInteger";
 
-// AddTwoNumber()
-AddTwoNumbers()
+ReverseInteger()

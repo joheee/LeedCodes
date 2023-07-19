db67dec6fc1dd342532a4e13e14df5c51e11c2b3
joheee <94522173+joheee@users.noreply.github.com>
2023-07-19 10:31:21 +0700

    foldering: grouping to directory Easy, Medium, Hard

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { AddTwoNumbers } from "./code/2_AddTwoNumbers";
-import { LongestSubstringWithoutDuplicate } from "./code/3_LongestSubstringWithoutDuplicate";
-import { ReverseInteger } from "./code/7_ReverseInteger";
-import { WordSearch } from "./code/79_WordSearch";
-import { AddTwoNumber } from "./code/445_AddTwoNumber";
+import { AddTwoNumbers } from "./code/Medium/2_AddTwoNumbers";
+import { LongestSubstringWithoutDuplicate } from "./code/Medium/3_LongestSubstringWithoutDuplicate";
+import { ReverseInteger } from "./code/Medium/7_ReverseInteger";
+import { WordSearch } from "./code/Medium/79_WordSearch";
+import { AddTwoNumber } from "./code/Medium/445_AddTwoNumber";
 
 LongestSubstringWithoutDuplicate()
